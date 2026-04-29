ef42e2b0a457d3ab979a720708a6a0006fba4bca
KingDarBoja <30560560+KingDarBoja@users.noreply.github.com>
2026-04-29 16:19:57 -0500

    feat: generate achievements ui library.

---
+++ libs/achievements/ui/src/index.ts
@@ -0,0 +1,1 @@
+export * from './lib/achievements-ui/achievements-ui';
---
+++ libs/achievements/ui/src/lib/achievements-ui/achievements-ui.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-achievements-ui',
+  imports: [],
+  templateUrl: './achievements-ui.html',
+  styleUrl: './achievements-ui.scss',
+})
+export class AchievementsUi {}
