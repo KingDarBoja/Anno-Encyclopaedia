a7b15f0dd1b8fc21fbb44decffed7d983e6ff389
KingDarBoja <30560560+KingDarBoja@users.noreply.github.com>
2026-05-06 11:01:37 -0500

    styles: city status card on mobile is more concise

--- libs/city-status/ui/src/lib/city-status-card/city-status-card.component.ts
+++ libs/city-status/ui/src/lib/city-status-card/city-status-card.component.ts
@@ -4,14 +4,16 @@ import {
   ChangeDetectionStrategy,
   inject,
 } from '@angular/core';
-import { NgTemplateOutlet } from '@angular/common';
 
-import { CityStatusService, CityStatusViewModel } from '@anno/city-status-data';
+import {
+  CityAttributeEffects,
+  CityStatusService,
+  CityStatusViewModel,
+} from '@anno/city-status-data';
 
 @Component({
   selector: 'anno-city-status-card',
   standalone: true,
-  imports: [NgTemplateOutlet],
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './city-status-card.component.html',
   styleUrl: './city-status-card.component.scss',
@@ -21,6 +23,28 @@ export class CityStatusCardComponent {
 
   readonly iconBase = 'assets/icons/main/attributes/';
   readonly data = input.required<CityStatusViewModel>();
+
+  // Cast the array so 'key' matches the keys of CityAttributeEffects
+  readonly attributes = [
+    { key: 'belief', label: 'Belief', icon: 'icon_religion_belief_0.webp' },
+    {
+      key: 'knowledge',
+      label: 'Knowledge',
+      icon: 'icon_techtree_knowledge_0.webp',
+    },
+    { key: 'prestige', label: 'Prestige', icon: 'icon_prestige_0.webp' },
+    { key: 'health', label: 'Health', icon: 'icon_health_0.webp' },
+    {
+      key: 'fire_safety',
+      label: 'Fire Safety',
+      icon: 'icon_fire_safety_0.webp',
+    },
+    { key: 'happiness', label: 'Happiness', icon: 'icon_happiness_0.webp' },
+  ] as const satisfies {
+    key: keyof CityAttributeEffects;
+    label: string;
+    icon: string;
+  }[];
 
   readonly placeholderImage =
     'assets/icons/base/icon_content/city_status/icon_2d_city_status_01_0.webp';
